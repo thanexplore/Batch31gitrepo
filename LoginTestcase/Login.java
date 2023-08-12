a4e315475109441df328594297d7e34be9982b03
than <thanexplore@gmail.com>
2023-08-12 19:32:27 +0530

    login testcase completed

---
+++ LoginTestcase/Login.java
@@ -0,0 +1,9 @@
+package LoginTestcase;
+
+public class Login {
+    public static void main(String[] args) {
+        //script for login test case
+        //calculate / additon for any two numbers
+
+    }
+}
